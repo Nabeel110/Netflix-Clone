246abd61e9322a93e629872fb0e607faabdb7edb
Nabeel Ahmed <nabeelmoolji1@gmail.com>
2021-02-05 14:57:56 +0500

    Added Logo Locally

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import logo from "../Images/logo.png";
 import "./Nav.css";
 
 const Nav = () => {
@@ -21,11 +22,7 @@ const Nav = () => {
   return (
     <div className={`nav ${show && "nav_black"}`}>
       <div className="nav_contents">
-        <img
-          className="nav_logo"
-          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
-          alt=""
-        ></img>
+        <img className="nav_logo" src={logo} alt=""></img>
         <img
           className="nav_avatar"
           src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
